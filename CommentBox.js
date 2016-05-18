d3b2fc4c034e5bcbd7c95751db9774a618fb911a
Pablo Krause <pablo.krause@tenser.mx>
2016-05-18 10:19:12 -0500

    Composing components

--- CommentBox.js
+++ CommentBox.js
@@ -5,7 +5,9 @@ class CommentBox extends React.Component {
     render() {
         return (
           <div className="commentBox">
-            Hello, world! I am a CommentBox.
+            <h1>Comments</h1>
+            <CommentList />
+            <CommentForm />
           </div>
         );
     }
@@ -13,5 +15,25 @@ class CommentBox extends React.Component {
 
 // Alternative statless component (defined as a function):
 // const CommentBox = () => <div className="commentBox">Hello, world! I am a CommentBox.</div>
+
+class CommentList extends React.Component {
+    render() {
+        return (
+          <div className="commentList">
+            Hello, world! I am a CommentList.
+          </div>
+        );
+    }
+}
+
+class CommentForm extends React.Component {
+    render() {
+        return (
+          <div className="commentForm">
+            Hello, world! I am a CommentForm.
+          </div>
+        );
+    }
+}
 
 export default CommentBox
