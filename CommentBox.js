c4e95ac287d4d18c0b3df2263c99048d41561010
Pablo Krause <pablo.krause@tenser.mx>
2016-05-18 17:46:40 -0500

    Fetching from the server

--- CommentBox.js
+++ CommentBox.js
@@ -2,12 +2,36 @@ import React from 'react';
 import marked from 'marked';
 
 // Stateful component (defined as a class):
-class CommentBox extends React.Component {
+export default class CommentBox extends React.Component {
+
+  // For ES6 classes, getInitialState has been deprecated in favor of declaring
+  //  an initial state object in the constructor
+  constructor(props, context) {
+    super(props, context);
+    this.state = {data: []};
+  }
+
+  loadCommentsFromServer() {
+    // Using github's fetch polyfill instead of jQuery
+    fetch(this.props.url, {
+    }).then(res => {
+        return res.json();
+    }).then(data => {
+      this.setState({data: data});
+    });
+  }
+
+  componentDidMount() {
+    this.loadCommentsFromServer();
+    // The original tutorial is missing the bind(this)
+    setInterval(this.loadCommentsFromServer.bind(this), this.props.pollInterval);
+  }
+
   render() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data} />
+        <CommentList data={this.state.data} />
         <CommentForm />
       </div>
     );
@@ -16,8 +40,7 @@ class CommentBox extends React.Component {
 
 // Alternative statless component (defined as a function):
 // const CommentBox = () => <div className="commentBox">Hello, world! I am a CommentBox.</div>
-
-export default CommentBox
+// export default CommentBox
 
 class CommentList extends React.Component {
   render() {
--- main.js
+++ main.js
@@ -2,10 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentBox from './CommentBox';
 
-// Eventually this will come from the server
-var data = [
-  {id: 1, author: "Pete Hunt", text: "This is one comment"},
-  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
-];
-
-ReactDOM.render(<CommentBox data={data}/>, document.getElementById('content'));
+ReactDOM.render(<CommentBox url="/api/comments" pollInterval="5000" />, document.getElementById('content'));
