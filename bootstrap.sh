ef12771a78886092b5ab0501ff6944418bbd5a75
Pablo Krause <pablo.krause@tenser.mx>
2016-05-18 09:13:45 -0500

    Bootstrap script for creating a project
    using react, webpack and babel.

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Prerequisite:
+# sudo npm install babel webpack webpack-dev-server -g
+
+npm init -y
+npm install react react-dom --save
+npm install babel-loader babel-core babel-preset-es2015 babel-preset-react
