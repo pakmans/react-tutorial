676168f2c330acd20c1a2dc63d528391e21de3b0
Pablo Krause <pablo.krause@tenser.mx>
2016-05-18 14:23:24 -0500

    Using props

--- CommentBox.js
+++ CommentBox.js
@@ -2,38 +2,53 @@ import React from 'react';
 
 // Stateful component (defined as a class):
 class CommentBox extends React.Component {
-    render() {
-        return (
-          <div className="commentBox">
-            <h1>Comments</h1>
-            <CommentList />
-            <CommentForm />
-          </div>
-        );
-    }
+  render() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
 }
 
 // Alternative statless component (defined as a function):
 // const CommentBox = () => <div className="commentBox">Hello, world! I am a CommentBox.</div>
 
+export default CommentBox
+
 class CommentList extends React.Component {
-    render() {
-        return (
-          <div className="commentList">
-            Hello, world! I am a CommentList.
-          </div>
-        );
-    }
+  render() {
+    return (
+      <div className="commentList">
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
+      </div>
+    );
+  }
 }
 
 class CommentForm extends React.Component {
-    render() {
-        return (
-          <div className="commentForm">
-            Hello, world! I am a CommentForm.
-          </div>
-        );
-    }
+  render() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
 }
 
-export default CommentBox
+class Comment extends React.Component {
+  render() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  };
+}
+
